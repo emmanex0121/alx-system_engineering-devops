67c9f41d435447527dccac00d72eb69d946ef5a4
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-03-25 18:36:01 +0100

    hot fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,12 @@
 #!/usr/bin/pup
 # installing a specific version of flask 2.1.0
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'Flask':
+  command => '/usr/bin/pip3 install flask=2.1.0',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
 }
